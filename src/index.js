4780c895c98c35a634b9ea80f264993323f71333
qinzhen <qz519189636@qq.com>
2023-05-06 16:52:15 +0800

    feat: update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { open } = require("fs/promises")
 const path = require('path');
 const { parse } = require("@babel/parser");
 const generate = require("@babel/generator").default
@@ -14,17 +15,50 @@ function main() {
 }
 
 
-function ASTDelSSOCode(filePath) {
-  let code = fs.readFileSync(p, 'utf-8');
+async function ASTDelSSOCode(filePath) {
+  let code = fs.readFileSync(filePath, 'utf-8');
   const ast = parse(code);
-  const output = generate(
-    ast,
-    {
-      // ...options
-    },
-    code
-  );
-  fs.writeFileSync(outputPath, output.code);
+  const { comments } = ast
+  let start = 0
+  let end = 0
+  for (let i = 0;i < comments.length;i++) {
+    const comment = comments[i]
+    if (comment.value.includes('SSO')) {
+      const { loc } = comment
+      if (start === 0) {
+        start = loc.start.line
+      } else {
+        end = loc.start.line
+      }
+    }
+  }
+
+  console.log(code)
+
+  // filehandle = await open(filePath);
+  // for await (const line of filehandle.readLines()) {
+  //   console.log(line);
+  // }
+
+
+
+  // const res = traverse(ast, {
+  //   // CommentBlock(path) {
+  //   //   console.log(path.node)
+  //   // }
+  // });
+  // console.log(ast)
+  // const output = generate(
+  //   ast,
+  //   {
+  //     // ...options
+  //   },
+  //   code
+  // );
+  // fs.writeFileSync(outputPath, output.code);
+
+
+
 }
 
 
